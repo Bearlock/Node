295c404e2c3b7bdd38184b4bf7dcfb2a25b58794
Bearlock <1erickdiaz@gmail.com>
2015-07-30 12:05:27 -0600

    Possible spacing issues fix

--- program7.js
+++ program7.js
@@ -4,9 +4,9 @@ var site = process.argv[2];
 // Executes http GET on a site. Takes site response as callback.
 // Encodes response to utf8 and prints it to stdout
 http.get(site, function callback(response) {
-	response.setEncoding("utf8");
-	response.on("data", function(data) {
-		console.log(data);
-	});
-	response.on('error', console.error );
+    response.setEncoding("utf8");
+    response.on("data", function(data) {
+        console.log(data);
+    });
+    response.on('error', console.error );
 });
