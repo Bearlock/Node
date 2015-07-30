6fc15c63681ef37dbe8867af1a778dd5aac3eb29
Bearlock <1erickdiaz@gmail.com>
2015-07-30 10:35:07 -0600

    8th learnyounode exercise

---
+++ program8.js
@@ -0,0 +1,19 @@
+var http = require('http');
+var site = process.argv[2];
+var text = '';
+
+// Executes http GET on a site. Takes site response as callback.
+// Encodes response to utf8 and concatenates it all to text var.
+// Prints out length of string and then prints out datastring. 
+http.get(site, function callback(response) {
+	response.setEncoding("utf8");
+	response.on("data", function(data) {
+		text += data;
+	});
+	response.on('error', console.error );
+	response.on('end', function() {
+		console.log(text.length);
+		console.log(text);
+	});
+});
+
