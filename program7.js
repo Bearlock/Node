6bba23b968669769d812f3997ca26119f729e6c3
Bearlock <1erickdiaz@gmail.com>
2015-07-30 09:15:15 -0600

    Added some commenting and removed unneccesary code

--- program7.js
+++ program7.js
@@ -1,11 +1,11 @@
 var http = require('http');
 var site = process.argv[2];
 
+// Executes http GET on a site. Takes site response as callback.
+// Encodes response to utf8 and prints it to stdout
 http.get(site, function callback(response) {
 	response.setEncoding("utf8");
 	response.on("data", function(data) {
-	//	for (stuff in data) {
 			console.log(data);
-       // }
 	});
 });
